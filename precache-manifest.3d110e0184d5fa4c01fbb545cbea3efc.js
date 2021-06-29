23222d2d77215e7b468ecdaba9c48257171b1383
jinweihuang <jinweihuang@gpmcorp.com.tw>
2021-06-30 01:57:18 +0800

    test

---
+++ precache-manifest.3d110e0184d5fa4c01fbb545cbea3efc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c6fb6f51f6f3d10e62",
+    "url": "/css/app.a7c732cd.css"
+  },
+  {
+    "revision": "13d6f8a53c00b49cc2b7",
+    "url": "/css/chunk-vendors.60bfa2d6.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3f83eefbf5dcebe6dff977aa74f2c6ea",
+    "url": "/img/collection/IDMS-1.png"
+  },
+  {
+    "revision": "13c59dc4694d198c8668aba36ebbd1f1",
+    "url": "/img/collection/IDMS-2.png"
+  },
+  {
+    "revision": "38d4965d457024489a7a4c113536216e",
+    "url": "/img/collection/IDMS-3.png"
+  },
+  {
+    "revision": "56946c901d2552ac132bb70594d42fc6",
+    "url": "/img/collection/IDMS-4.png"
+  },
+  {
+    "revision": "7953447ec353e4ee7bbc7c56d9c6aa84",
+    "url": "/img/collection/IDMS-5.png"
+  },
+  {
+    "revision": "44733c539f5c222e397cca5513579b67",
+    "url": "/img/collection/data_gateway/1.PNG"
+  },
+  {
+    "revision": "68503374ae8102a195338dabb1ca7654",
+    "url": "/img/collection/data_gateway/2.PNG"
+  },
+  {
+    "revision": "c7da870bdde6bdcac4f0ad8cf2629afe",
+    "url": "/img/collection/data_gateway/3.PNG"
+  },
+  {
+    "revision": "fd096631b9f92ce092bee017b1444123",
+    "url": "/img/collection/data_gateway/4.png"
+  },
+  {
+    "revision": "e2f03a08d9ee941745e22036fd3bec79",
+    "url": "/img/collection/ginwei.png"
+  },
+  {
+    "revision": "3216f3671eeadd9ea054eab0e3d383df",
+    "url": "/img/collection/idms_home.png"
+  },
+  {
+    "revision": "7125c0e11dacaf3e52ca1dc91f7f6a9a",
+    "url": "/img/collection/idms_model.png"
+  },
+  {
+    "revision": "b97260e19cc42257931f5680dbed50f0",
+    "url": "/img/collection/landService-1.png"
+  },
+  {
+    "revision": "a2fcbe97bee2d30f3cc70a660d4ee85b",
+    "url": "/img/collection/landService-2.png"
+  },
+  {
+    "revision": "12e0cfecaa31f6ca0a26d8375a53381e",
+    "url": "/img/collection/landService-3.png"
+  },
+  {
+    "revision": "b7f20fdf5ba8be91a71ab13daf8a125e",
+    "url": "/img/collection/landService.png"
+  },
+  {
+    "revision": "ac46684a0a55e8b8f4eeb102ca107fb3",
+    "url": "/img/collection/order_system-1.png"
+  },
+  {
+    "revision": "705ca1f74f2854a9c842ab1a4dcc6532",
+    "url": "/img/collection/order_system-2.png"
+  },
+  {
+    "revision": "a77ca4dec16f861af9a5921f79bcaa51",
+    "url": "/img/collection/order_system-3.png"
+  },
+  {
+    "revision": "ce2294db016bb4f24b569245189abb05",
+    "url": "/img/collection/order_system-4.png"
+  },
+  {
+    "revision": "2646e9bda3a43bb0223687faf04bade1",
+    "url": "/img/collection/personalResume.png"
+  },
+  {
+    "revision": "11a21d7eb35c1def30cad9b5f08a3958",
+    "url": "/img/collection/spaceX_api_call_1.png"
+  },
+  {
+    "revision": "e2f03a08d9ee941745e22036fd3bec79",
+    "url": "/img/ginwei.e2f03a08.png"
+  },
+  {
+    "revision": "44cb7f55d0dbfc42dcc75ebca38ddc0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c6fb6f51f6f3d10e62",
+    "url": "/js/app.a1c9a205.js"
+  },
+  {
+    "revision": "13d6f8a53c00b49cc2b7",
+    "url": "/js/chunk-vendors.ad31e5a4.js"
+  },
+  {
+    "revision": "096f5949a95593e1fa7e4a2cb243d057",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
